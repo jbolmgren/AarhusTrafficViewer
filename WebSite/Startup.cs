64830a658feb17a666b70f0ad1403bc31eda5f7d
Jonas <Jonas@bolmgren.com>
2016-11-27 21:18:03 +0100

    Rename Folder

---
+++ WebSite/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Nancy.Owin;
+using Owin;
+using WebSite;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace WebSite
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            SetupApi(app);
+            SetupWeb(app);
+        }
+
+        private static void SetupApi(IAppBuilder app)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+            DependencyInjectionConfig.Register(httpConfiguration);
+            app.UseWebApi(httpConfiguration);
+        }
+
+        private static void SetupWeb(IAppBuilder app)
+        {
+            app.UseNancy(new NancyOptions());
+        }
+    }
+}
